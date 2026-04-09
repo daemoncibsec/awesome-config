b0810f0610a3222af6f137ec05e6e05097d840eb
daemoncibsec <daemoncibsec@gmail.com>
2026-04-09 13:47:20 +0200

    Fixed syntax errors on installation.sh

--- installation.sh
+++ installation.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Ensure you have sudo permissions before starting the script. Otherwise, of course, will not work.
-
 update_sys() {
 	echo "\nBeginning system update."
 	sudo apt update
@@ -20,18 +18,21 @@ update_sys() {
 		return 1
 	fi
 
-	if [[ $? -eq 0 ]]; then
+	if [[ $? -ne 0 ]]; then		
 		return 1
 	fi
-	return 
+
+	return 0
 }
 
 install_packages() {
 	echo "\nInstalling packages"
 	sudo apt install awesome librewolf tilix kitty picom neovim flameshot thunar ranger gcc python3 fzf -y
-	if [[ $? -eq 0 ]]; then
+	if [[ $? -neq 0 ]]; then		
 		return 1
 	fi
+
+	return 0
 }
 
 copy_configurations() {
@@ -40,7 +41,7 @@ copy_configurations() {
 	cp -r awesome ~/.config/awesome
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring Kitty Terminal..."
+		echo "\nConfiguring Kitty Terminal..."
 		mkdir ~/.config/kitty &>/dev/null
 		cp -r kitty ~/.config/kitty
 	else
@@ -48,14 +49,14 @@ copy_configurations() {
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring bash prompt..."
+		echo "\nConfiguring bash prompt..."
 		cp bashrc ~/.bashrc
 	else
 		return 1
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring window borders..."
+		echo "\nConfiguring window borders..."
 		mkdir -p ~/.config/gtk-3.0/ &>/dev/null
 		cp gtk.css ~/.config/gtk-3.0/gtk.css
 	else
@@ -63,7 +64,7 @@ copy_configurations() {
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring NeoVim Text Editor..."
+		echo "\nConfiguring NeoVim Text Editor..."
 		mkdir ~/.config/nvim &>/dev/null
 		cp -r nvim ~/.config/nvim
 	else
@@ -71,7 +72,7 @@ copy_configurations() {
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring Thunar File Explorer..."
+		echo "\nConfiguring Thunar File Explorer..."
 		mkdir ~/.themes/ &>/dev/null
 		cp -r themes/* ~/.themes
 	else
@@ -79,7 +80,7 @@ copy_configurations() {
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Enabling touchpad compatibility for laptops..."
+		echo "\nEnabling touchpad compatibility for laptops..."
 		mkdir -p /etc/X11/xorg.conf.d/ &>/dev/null
 		sudo cp 30-touchpad.conf /etc/X11/xorg.conf.d/30-touchpad.conf
 	else
@@ -87,39 +88,45 @@ copy_configurations() {
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring Bash aliases..."
+		echo "\nConfiguring Bash aliases..."
 		cp bash_aliases ~/.bash_aliases
 	else
 		return 1
 	fi
 
 	if [[ $? -eq 0 ]]; then
-		echo "Configuring Btop process manager..."
+		echo "\nConfiguring Btop process manager..."
 		cp -r btop ~/.config/btop
+	else
 		return 1
 	fi
+
+	if [[ $? -neq 0 ]]; then		
+		return 1
+	fi
+
 	return 0
 }
 
 main() {
-	if [[ $EUID -eq 0]]; then
+	if [[ $EUID -eq 0 ]]; then
 		update_sys
 		if [[ $? -eq 0 ]]; then
 			install_packages
 			if [[ $? -eq 0 ]]; then
 				copy_configurations
 			else
-				echo "An error occured while copying configurations. Aborting."
+				echo "\nAn error occured while copying configurations. Aborting."
 				return 1
 			fi
 		else
-			echo "Couldn't update system. Aborting installation."
+			echo "\nCouldn't update system. Aborting installation."
 			return 1
 		fi
 		chown -R $USER:$USER /home/$USER
-		echo "Installation completed. Make sure to restart so the changes can apply."
+		echo "\nInstallation completed. Make sure to restart so the changes can apply."
 	else
-		echo "You must run this script with root privileges."
+		echo "\nYou must run this script with root privileges."
 		return 1
 	fi
 }
